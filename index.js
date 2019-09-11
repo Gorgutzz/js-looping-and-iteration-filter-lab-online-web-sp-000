6dbeb995590c1eea4a9fb4a6299605e576e3e7eb
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-11 21:13:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file
+function filter (collection) {
+  for (const user of collection) {
+    if (likesElephants(user)) {
+      console.log(user.firstName);
+    }
+  }
+}
+ 
