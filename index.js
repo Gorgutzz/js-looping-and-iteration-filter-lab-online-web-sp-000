3c70db420675d0b75fcdadc0527f0354a53a7783
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-11 21:29:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solution in this file
 function findMatching(drivers, name) {
-    return drivers.filter(function (driver) { return driver.toLowerCase() === name.toLowerCase(); }) ;
+  return drivers.filter(function (driver) { return driver.toLowerCase() === name.toLowerCase(); }) ;
 }
 
-function fuzzyMatch(drivers, name)
+function fuzzyMatch(drivers, name) {
+  return drivers.filter(function (driver) { return user.startsWith(name) ; }) ;
+}
